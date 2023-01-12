312e2c1df2106c265e407a346f5a243a9d56ca12
kousiknandy <kousiknandy@gmail.com>
2023-01-12 23:55:42 +0000

    Add model as a struct and cast the response to it

--- gptrust_proxy/src/main.rs
+++ gptrust_proxy/src/main.rs
@@ -2,7 +2,7 @@
 
 async fn main() {
     println!("Hello, world!\nAnd ");
-    gptrust_api::gptrust_getmodels()
+    gptrust_api::models::gptrust_getmodels()
         .await
         .expect("Failed to get models");
 }
