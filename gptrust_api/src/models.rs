5dda2dcfbd1b55913f5d2458652d15f7ef3807cf
kousiknandy <kousiknandy@users.noreply.github.com>
2023-01-13 16:28:37 +0000

    Move http calls to a separate file (#3)

--- gptrust_api/src/models.rs
+++ gptrust_api/src/models.rs
@@ -1,6 +1,3 @@
-use std::env;
-use hyper;
-use hyper_tls;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -38,25 +35,7 @@ struct ModelResponse {
        
 pub async fn gptrust_getmodels()  ->  Result<(), Box<dyn std::error::Error>> {
 
-    let https = hyper_tls::HttpsConnector::new();
-    let client = hyper::Client::builder().build::<_, hyper::Body>(https);
-    let api_key = env::var("OPENAI_API_KEY").expect("$OPENAI_API_KEY is not set");
-    let req = hyper::Request::builder()
-        .method(hyper::Method::GET)
-        .uri("https://api.openai.com/v1/models")
-        .header("Authorization", format!("Bearer {}", api_key))
-        .header("OpenAI-Organization", "org-ioVS0wAWUCPVBK4x45pqIGCj")
-        .body(hyper::Body::from(""))?;
-
-    // Pass our request builder object to our client.
-    let resp = client.request(req).await?;
-
-    // Get the response body bytes.
-    let body_bytes = hyper::body::to_bytes(resp.into_body()).await?;
-
-    // Convert the body bytes to utf-8
-    let body = String::from_utf8(body_bytes.to_vec()).unwrap();
-
+    let body = gptrust_http::openai_http::openai_get("models".to_string()).await?;
     let model_resp: ModelResponse = serde_json::from_str(&body)?;
     println!("{:#?}", model_resp.data[0]);
 
