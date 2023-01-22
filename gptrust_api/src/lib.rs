ec2c944f352829628a2e74d35b7a6a57fcdae0dc
kousiknandy <kousiknandy@gmail.com>
2023-01-22 23:18:11 +0000

    Added the first text completion support

--- gptrust_api/src/lib.rs
+++ gptrust_api/src/lib.rs
@@ -1,2 +1,3 @@
-pub mod models;
+pub mod completions;
 pub mod engines;
+pub mod models;
--- gptrust_http/src/openai_http.rs
+++ gptrust_http/src/openai_http.rs
@@ -27,3 +27,29 @@ pub async fn openai_get(subpath: String) -> Result<String, Box<dyn std::error::E
 
     Ok(body)
 }
+
+pub async fn openai_post(
+    subpath: String,
+    body: String,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let full_path = format!("{}{}", OPENAI_BASE_URL, subpath);
+    let https = hyper_tls::HttpsConnector::new();
+    let client = hyper::Client::builder().build::<_, hyper::Body>(https);
+    let api_key = env::var(OPENAI_ENV).expect("$OPENAI_API_KEY is not set");
+    let req = hyper::Request::builder()
+        .method(hyper::Method::POST)
+        .uri(full_path)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .header("OpenAI-Organization", OPENAI_ORG)
+        .header("Content-type", "application/json")
+        .body(hyper::Body::from(body))?;
+
+    // Pass our request builder object to our client.
+    let resp = client.request(req).await?;
+    // Get the response body bytes.
+    let body_bytes = hyper::body::to_bytes(resp.into_body()).await?;
+    // Convert the body bytes to utf-8
+    let body: String = String::from_utf8(body_bytes.to_vec()).unwrap();
+
+    Ok(body)
+}
