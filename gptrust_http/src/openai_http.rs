795bb6c3217e5c4a4816d828f4c2f331b2f92f5a
Kousik Nandy <kousiknandy@gmail.com>
2023-03-23 22:07:03 +0000

    add support to post form in http lib

--- gptrust_http/src/openai_http.rs
+++ gptrust_http/src/openai_http.rs
@@ -69,3 +69,78 @@ pub async fn openai_post(
         ))),
     }
 }
+
+fn add_field(boundary: String, field: String, value: String) -> io::Result<Vec<u8>> {
+    let mut data = Vec::new();
+    write!(data, "--{}\r\n", boundary)?;
+    write!(
+        data,
+        "Content-Disposition: form-data; name=\"{}\"\r\n",
+        field
+    )?;
+    write!(data, "Content-type: text/plain;charset=UTF-8\r\n")?;
+    write!(data, "\r\n")?;
+    write!(data, "{}", value.into_bytes())?;
+    write!(data, "\r\n")?;
+    Ok(data)
+}
+
+fn add_boundary(boundary: String) -> io::Result<Vec<u8>> {
+    let mut data = Vec::new();
+    write!(data, "--{}--\r\n", boundary)?;
+    Ok(data)
+}
+
+fn add_file(boundary: String, filename: String) -> io::Result<Vec<u8>> {
+    let mut data = Vec::new();
+    write!(data, "--{}\r\n", boundary)?;
+    write!(
+        data,
+        "Content-Disposition: form-data; name=\"file\"; filename=\"{}\"\r\n",
+        filename
+    )?;
+    write!(data, "Content-Type: application/octet-stream\r\n")?;
+    write!(data, "\r\n")?;
+    let mut f = File::open(filename)?;
+    f.read_to_end(&mut data)?;
+    write!(data, "\r\n")?;
+    Ok(data)
+}
+
+pub async fn openai_post_form(
+    subpath: String,
+    fields: Vec<String>,
+    values: Vec<String>,
+    filename: Option<String>,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let full_path = format!("{}{}", OPENAI_BASE_URL, subpath);
+    let https = hyper_tls::HttpsConnector::new();
+    let client = hyper::Client::builder().build::<_, hyper::Body>(https);
+    let api_key = env::var(OPENAI_ENV).expect("$OPENAI_API_KEY is not set");
+    let req = hyper::Request::builder()
+        .method(hyper::Method::POST)
+        .uri(full_path)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .header("OpenAI-Organization", OPENAI_ORG)
+        .header(
+            "Content-type",
+            format!("multipart/form-data; boundary={}", boundary),
+        )?;
+
+    // Pass our request builder object to our client.
+    let resp = client.request(req).await?;
+    // println!("{:#?}", resp);
+    match resp.status().is_success() {
+        true => {
+            // Get the response body bytes.
+            let body_bytes = hyper::body::to_bytes(resp.into_body()).await?;
+            // Convert the body bytes to utf-8
+            let body: String = String::from_utf8(body_bytes.to_vec()).unwrap();
+            Ok(body)
+        }
+        false => Err(From::from(format!(
+            "Request failed, reason: {}",
+            resp.status()
+        ))),
+    }
+}
