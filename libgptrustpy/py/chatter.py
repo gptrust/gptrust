1339115131149b5bf6543f8fb5b95b6590f35564
Kousik Nandy <kousiknandy@gmail.com>
2023-06-07 22:51:25 +0000

    migrate python binding to pyo3

--- libgptrustpy/py/chatter.py
+++ libgptrustpy/py/chatter.py
@@ -2,7 +2,7 @@ import gptrustpy
 
 def main():
     inline = input("¯\_(ツ)_/¯ ? ")
-    outline = gptrustpy.chat_complete(inline)
+    outline = gptrustpy.answer(inline)
     print("<|°_°|>", outline)
     return 0
 
